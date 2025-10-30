ba1f3ef37744cbaee9f410134bb512ff4dab6efa
listaGDGroup <lista.gdgroup@gmail.com>
2025-10-30 02:25:29 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -56,9 +56,7 @@ export default {
                         </li>
                     </ul>
                     <h2>Records</h2>                   
-                    <p v-if="selected + 1 >= 50">This level does not accept new records.</p>
-                    <p v-else-if="selected +1 <= 25"><strong>100%</strong> or better to qualify</p>
-                    <p v-else>This level does not accept new records.</p>
+                    <p v-if="selected + 1 > 50">This This level does not accept new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
                             <td class="percent">
