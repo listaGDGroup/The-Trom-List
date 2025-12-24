d8a08c91ea2a630c5534aba0fa0498354d19b1dc
listaGDGroup <lista.gdgroup@gmail.com>
2025-12-24 07:54:20 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,8 +51,8 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
+                            <div class="type-title-sm">Nong</div>
+                            <p>{{ level.nong }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>                   
